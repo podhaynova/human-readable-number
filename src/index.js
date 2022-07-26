c30ed1560426c7e711452edfcc264ebdf0e43339
podhaynova <99250018+podhaynova@users.noreply.github.com>
2022-07-26 20:07:28 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
   
+    const units = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 
+                    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const teens = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred  = ['hundred']
+    
+
+    
+    let strNum = '';
+    if (number === 0) {
+        return 'zero';
+    }
+    
+
+    if (Math.floor(number / 100) > 0) {
+        strNum = strNum + units[Math.floor(number / 100) - 1] + " " + hundred;
+        number = number - (Math.floor(number / 100) * 100);    
+    }        
+    
+    if (Math.floor(number / 10) == 2) {
+        strNum = strNum + ' ' +  teens[Math.floor(number / 10) - 1];
+        number = number - (Math.floor(number / 10) * 10);    
+    }       
+    if (number > 0) {
+        strNum = strNum + ' ' + units[number - 1];     
+        number = number - number;
+    }
+
+    return strNum;
 }
+
+
